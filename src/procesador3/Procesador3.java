de71f80c6faaf3f2f843433a28c1bdadc4851d30
Monica <hola.monibe@gmail.com>
2025-08-24 12:02:21 +0200

    JMenuItem de tamaños, camiados por JRadioButtonMenuItem. Método configura_menu reservado para las fuentes

--- src/procesador3/Procesador3.java
+++ src/procesador3/Procesador3.java
@@ -2,9 +2,8 @@ package procesador3;
 
 import javax.swing.*;
 import javax.swing.text.StyledEditorKit;
-
 import java.awt.*;
-import java.awt.event.*;
+
 
 public class Procesador3 {
 
@@ -34,15 +33,14 @@ class LaminaProcesador extends JPanel{
 		fuente= new JMenu("Fuente");
 		estilo= new JMenu("Estilo");
 		tamagno= new JMenu("Tamaño");
+		//--------------------------------------------------------
 		//Creación de los elementos del menú
-		configura_menu("Arial", "fuente", "Arial", 9, 10, "");
-		configura_menu("Courier", "fuente", "Courier", 9, 10, "");
-		configura_menu("Verdana", "fuente", "Verdana", 9, 10,"");
+		configura_menu("Arial", "fuente", "Arial", 9, 10);
+		configura_menu("Courier", "fuente", "Courier", 9, 10);
+		configura_menu("Verdana", "fuente", "Verdana", 9, 10);
 		
 		//--------------------------------------------------------
-//		configura_menu("Negrita", "estilo", "", Font.BOLD, 1,"src/procesador3/imagenes/negrita.gif");
-//		configura_menu("Cursiva", "estilo", "", Font.ITALIC, 1, "src/procesador3/imagenes/cursiva.gif");
-		//creación JCheckboxMenuItem
+		//creación JCheckboxMenuItem para los estilos
 		JCheckBoxMenuItem negrita= new JCheckBoxMenuItem("Negrita", new ImageIcon("src/procesador3/imagenes/negrita.gif"));
 		JCheckBoxMenuItem cursiva= new JCheckBoxMenuItem("Cursiva", new ImageIcon("src/procesador3/imagenes/cursiva.gif"));
 		negrita.addActionListener(new StyledEditorKit.BoldAction() );
@@ -50,10 +48,24 @@ class LaminaProcesador extends JPanel{
 		estilo.add(negrita);
 		estilo.add(cursiva);
 		//----------------------------------------------------------
-		configura_menu("12", "tamaño", "", 9, 12, "");
-		configura_menu("16", "tamaño", "", 9, 16,"");
-		configura_menu("18", "tamaño", "", 9, 18,"");
-		configura_menu("20", "tamaño", "", 9, 20,"");
+		//creación de JRadioButtonMenuItem para los tamaños
+		ButtonGroup migrupo= new ButtonGroup();
+		JRadioButtonMenuItem tam12= new JRadioButtonMenuItem("12", true);
+		JRadioButtonMenuItem tam16= new JRadioButtonMenuItem("16", false);
+		JRadioButtonMenuItem tam18= new JRadioButtonMenuItem("18", false);
+		JRadioButtonMenuItem tam20= new JRadioButtonMenuItem("20", false);
+		tam12.addActionListener(new StyledEditorKit.FontSizeAction("cambiaTamaño", 12));
+		tam16.addActionListener(new StyledEditorKit.FontSizeAction("cambiaTamaño", 16));
+		tam18.addActionListener(new StyledEditorKit.FontSizeAction("cambiaTamaño", 18));
+		tam20.addActionListener(new StyledEditorKit.FontSizeAction("cambiaTamaño", 20));
+		migrupo.add(tam12);
+		migrupo.add(tam16);
+		migrupo.add(tam18);
+		migrupo.add(tam20);
+		tamagno.add(tam12);
+		tamagno.add(tam16);
+		tamagno.add(tam18);
+		tamagno.add(tam20);
 		//---------------------------------------------------------
 		barra.add(fuente);
 		barra.add(estilo);
@@ -70,8 +82,8 @@ class LaminaProcesador extends JPanel{
 		
 	}
 	
-	public void configura_menu(String rotulo, String menu, String tipo_letra, int estilos, int tam, String ruta_icono) {
-		JMenuItem elem_menu= new JMenuItem(rotulo, new ImageIcon(ruta_icono));
+	public void configura_menu(String rotulo, String menu, String tipo_letra, int estilos, int tam) {
+		JMenuItem elem_menu= new JMenuItem(rotulo);
 		if(menu=="fuente") {
 			fuente.add(elem_menu);
 			if(tipo_letra=="Arial") {
@@ -83,16 +95,7 @@ class LaminaProcesador extends JPanel{
 				elem_menu.addActionListener(new StyledEditorKit.FontFamilyAction("cambiaLetra", "Verdana"));
 
 			}
-//		}else if(menu=="estilo") {
-//			estilo.add(elem_menu);
-//			if(estilos==Font.BOLD) {
-//			elem_menu.addActionListener(new StyledEditorKit.BoldAction() );
-//			}else if(estilos==Font.ITALIC) {
-//				elem_menu.addActionListener(new StyledEditorKit.ItalicAction());
-//			}
-		}else if(menu=="tamaño") {
-			tamagno.add(elem_menu);
-			elem_menu.addActionListener(new StyledEditorKit.FontSizeAction("cambiaTamaño", tam));
+
 		}
 		
 	}
